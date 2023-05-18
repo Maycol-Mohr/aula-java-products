8be2c615f785f4cb1778fe4975cd7ed36c0257cf
Maycol Mohr <maycolmohr@yahoo.com.br>
2023-05-18 17:51:32 +0200

    parte 2 da aula projeto-produto

--- src/main/java/com/devsuperior/demo/entities/Product.java
+++ src/main/java/com/devsuperior/demo/entities/Product.java
@@ -1,11 +1,25 @@
 package com.devsuperior.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
 	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department deparment;
 	
 	public Product() {
---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_department (name) VALUES ('Tech');
+INSERT INTO tb_department (name) VALUES ('Pet');
+INSERT INTO tb_department (name) VALUES ('Car');
+
+INSERT INTO tb_product (name, price, department_id) VALUES ('Macbook Pro', 4000.0, 1);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Pc Gamer', 5000.0, 1);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Pet House', 300.0, 2);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Ferrari', 3000000.0, 3);
+INSERT INTO tb_product (name, price, department_id) VALUES ('BMW', 800000.0, 3);
+INSERT INTO tb_product (name, price, department_id) VALUES ('Audi', 600000.0, 3);
+
